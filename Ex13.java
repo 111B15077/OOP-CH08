22f4df62f4a1a8390014321ba3edc38ca1ed4446
王語鴻 <131112519+111B15077@users.noreply.github.com>
2023-09-22 11:49:36 +0800

    Update Ex13.java

--- Ex13.java
+++ Ex13.java
@@ -1,15 +1,12 @@
 class CCircle {
-    // (a) 計算圓形面積的多載函數，傳值型態為 double
     double area(double r) {
         return Math.PI * r * r;
     }
 
-    // (b) 計算圓形面積的多載函數，傳回值型態為 float
     float area(float r) {
         return (float)(Math.PI * r * r);
     }
 
-    // (c) 計算圓形面積的多載函數，傳回值型態為 double
     double area(int r) {
         return Math.PI * r * r;
     }
@@ -19,7 +16,6 @@ public class Ex13 {
     public static void main(String[] args) {
         CCircle circle = new CCircle();
 
-        // (d) 呼叫不同多載的area函數並印出面積
         double result1 = circle.area(2);
         System.out.println("半徑為2的圓形面積: " + result1);
 
